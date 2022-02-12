4f16f40c4a16bce8b2808878efaccea5cff2e494
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-12 08:20:19 -0800

    added serach filter function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 
 from .serializers import HelloSerializer
@@ -99,10 +100,13 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'method': 'DELETE'})
     
 class UserProfileViewSet(viewsets.ModelViewSet):
-    # /api/profile/ (GET, POST)
-    # /api/profile/:id (GET, POST, PUT, PATCH, DELETE)
+    # api/profile/ (GET, POST)
+    # api/profile/:id (GET, POST, PUT, PATCH, DELETE)
+    # api/profile/?search=<TERM>
     """Handle creating and updating profiles"""
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
     authentication_class = (TokenAuthentication,) # tuple
     permission_classes = (UpdateOwnProfile,) # tuple
+    filter_backends = (filters.SearchFilter,) # tuple
+    search_fields = ('name', 'email',) # tuple
